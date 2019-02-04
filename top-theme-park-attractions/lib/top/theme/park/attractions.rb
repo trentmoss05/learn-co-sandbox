29a908ca81b1edf44fa54942bc555ec137b96082
Trent Moss <trentmoss05@gmail.com>
2019-02-04 17:40:10 +0000

    first commit

---
+++ top-theme-park-attractions/lib/top/theme/park/attractions.rb
@@ -0,0 +1,12 @@
+require "top/theme/park/attractions/version"
+
+module Top
+  module Theme
+    module Park
+      module Attractions
+        class Error < StandardError; end
+        # Your code goes here...
+      end
+    end
+  end
+end
---
+++ top-theme-park-attractions/lib/top/theme/park/attractions/version.rb
@@ -0,0 +1,9 @@
+module Top
+  module Theme
+    module Park
+      module Attractions
+        VERSION = "0.1.0"
+      end
+    end
+  end
+end
